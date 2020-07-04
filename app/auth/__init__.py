b724cad25a9208135fe13b6ca3d13ece40cc6757
Oprincely <oprincely@gmail.com>
2020-07-04 15:32:13 +0100

    v4

---
+++ app/auth/__init__.py
@@ -0,0 +1,90 @@
+from flask import render_template, flash, redirect, url_for, Blueprint, url_for, redirect, request, session
+
+
+############### Microblog things
+from app.models import User,Post
+from app import db
+from .forms import LoginForm,RegistrationForm
+from flask_login import current_user, login_user,logout_user,login_required 
+ 
+from werkzeug.urls import url_parse
+#from datetime import datetime
+#from app.forms import ResetPasswordForm,ResetPasswordRequestForm
+#from .email import send_password_reset_email
+################################
+import datetime
+
+x = datetime.datetime.now()
+year_now = x.year
+
+bp = Blueprint('auth', __name__)
+
+@bp.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('users.users'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/reset_password_request.html',title='Reset Password', form=form)
+
+
+@bp.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('users.users'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('users.users'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/reset_password.html', form=form)
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.profile'))
+    form = LoginForm()
+    
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('auth.login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('main.profile')
+        return redirect(next_page)
+    
+    return render_template('auth/login.html', title='Sign In', form=form)
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.profile'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(firstname=form.firstname.data,middlename=form.middlename.data,lastname=form.lastname.data,
+                    dob=form.dob.data,tob=form.tob.data,pob=form.pob.data,mobile=form.mobile.data,number_of_questions=0,
+                    username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        #msg = 'Congratulations, you are now a registered user!'
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
+@bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, url_for, redirect, request, session
 
+from flask_login import login_required
+
 import datetime
 from app.genum import generate_numbers
 from app.hello import contains_y,jeff, life_number,your_personal,real,hearts_d,image_num,bddict,year_num,lesson,debt
@@ -17,20 +19,33 @@ def index():
     return render_template('index.html',year_now=year_now)
 
 @bp.route('/profile', methods=['GET', 'POST'])
-#@login_required
+@login_required
 def profile():
-    msg=''
+    msg = ''
+    username = current_user.username
+    user = User.query.filter_by(username=username).first()
     return render_template('profile.html',msg=msg,year_now=year_now)
 
 
 #-------------------Numerology Readings----------------------#
 
 @bp.route('/event')
-#@login_required
+@login_required
 def event():
+    username = current_user.username
+    user = User.query.filter_by(username=username).first()
     
+    FN = user.firstname
+    MN = user.middlename
+    LN = user.lastname
     
-    e = generate_numbers('uchechukwu','emeka','okwu','22','02','1982')[3]
+    btd = user.dob[0:2] #1982
+    btm = user.dob[3:5] #02
+    bty = user.dob[6:10] #22
+    
+    e = generate_numbers(FN,MN,LN,btd,btm,bty)[3]
+    
+    #e = generate_numbers('uchechukwu','emeka','okwu','22','02','1982')[3]
     
     return render_template('event.html',exp11=e[0],sU=e[1],iM1=e[2],lp=e[3],Mrity=e[4],phy=e[5],men=e[6],emo=e[7],intt=e[8],one=e[9],
                             two=e[10],thr=e[11],fou=e[12],fiv=e[13],six=e[14],sev=e[15],eig=e[16],nin=e[17],ps=e[18],cH=e[19],cH1=e[20],cH2=e[21],cH3=e[22],
@@ -39,11 +54,22 @@ def event():
 
 
 @bp.route('/reading')
-#@login_required
+@login_required
 def reading():
+    username = current_user.username
+    user = User.query.filter_by(username=username).first()
     
+    FN = user.firstname
+    MN = user.middlename
+    LN = user.lastname
     
-    r = generate_numbers('uchechukwu','emeka','okwu','22','02','1982')[0]
+    btd = user.dob[0:2] #1982
+    btm = user.dob[3:5] #02
+    bty = user.dob[6:10] #22
+    
+    r = generate_numbers(FN,MN,LN,btd,btm,bty)[0]
+    
+    #r = generate_numbers('uchechukwu','emeka','okwu','22','02','1982')[0]
     
     return render_template('reading.html', exp=r[0],exp11=r[1], sU=r[2], sU1=r[3], iM=r[4], iM1=r[5],
                            hP=r[6], ln=r[7], lp=r[8],bd1=r[9], lb=r[10], cH=r[11], cH1=r[12], cH2=r[13], cH3=r[14], LEB=r[15], 
@@ -55,11 +81,22 @@ def reading():
 
 
 @bp.route('/reading1')
-#@login_required
+@login_required
 def reading1():
+    username = current_user.username
+    user = User.query.filter_by(username=username).first()
     
+    FN = user.firstname
+    MN = user.middlename
+    LN = user.lastname
     
-    r = generate_numbers('uchechukwu','emeka','okwu','22','02','1982')[2]#[1]
+    btd = user.dob[0:2] #1982
+    btm = user.dob[3:5] #02
+    bty = user.dob[6:10] #22
+    
+    r = generate_numbers(FN,MN,LN,btd,btm,bty)[2]#[1]
+    
+    #r = generate_numbers('uchechukwu','emeka','okwu','22','02','1982')[2]#[1]
     
     return 'soon'#render_template('reading1.html',FN=FN,lp=lp,A=A,B=B,C=C,D=D,E=E,F=F,G=G,H=H,I=I,
                  #          Rall=Rall,R1all=R1all,R2all=R2all,R3all=R3all,R4all=R4all,R5all=R5all,R6all=R6all,R7all=R7all,
