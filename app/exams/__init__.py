ec0c8e1c62fdc8e55495c3b1de5657a6469c6380
Oprincely <oprincely@gmail.com>
2020-07-16 12:45:08 +0100

    v9

--- app/exams/__init__.py
+++ app/exams/__init__.py
@@ -11,85 +11,85 @@ def exams():
         
         answer1 = request.form['answer1']
         if answer1 == 'A,B,C,D':
-            marks.append(6.08)
+            marks.append(7.69)
         elif answer1 == 'A,B':
-            marks.append(3.04)
+            marks.append(3.076)
         elif answer1 == 'A':
-            marks.append(1.52)
+            marks.append(1.538)
         elif answer1 == 'B':
-            marks.append(1.52)
+            marks.append(1.538)
         else:
-            marks.append(4.56)
+            marks.append(4.614)
         
         answer2 = request.form['answer2']
         if answer2 == 'A':
-            marks.append(7.6)
+            marks.append(7.69)
         else:
             marks.append(0)
             
         answer3 = request.form['answer3']
         if answer3 == 'C':
-            marks.append(7.6)
+            marks.append(7.69)
         else:
             marks.append(0)
             
         answer4 = request.form['answer4']
         if answer4 == 'A,B,D':
-            marks.append(7.6)
+            marks.append(7.69)
         else:
             marks.append(0)
             
         answer5 = request.form['answer5']
         if answer5 == 'A,B,C':
-            marks.append(7.6)
+            marks.append(7.69)
         else:
             marks.append(0)
             
         answer6 = request.form['answer6']
         if answer6 == 'A,D':
-            marks.append(7.6)
+            marks.append(7.69)
         else:
             marks.append(0)
         
         answer7 = request.form['answer7']
         if answer7 == 'C':
-            marks.append(7.6)
+            marks.append(7.69)
         else:
             marks.append(0)
             
         answer8 = request.form['answer8']
         if answer8 == 'B':
-            marks.append(7.6)
+            marks.append(7.69)
         else:
             marks.append(0)
             
         answer9 = request.form['answer9']
         if answer9 == 'A':
-            marks.append(7.6)
+            marks.append(7.69)
         else:
             marks.append(0)
             
         answer10 = request.form['answer10']
         if answer10 == 'E':
-            marks.append(7.6)
+            marks.append(7.69)
         else:
             marks.append(0)
             
         answer11 = request.form['answer11']
         if answer11 == 'A':
-            marks.append(7.6)
+            marks.append(7.69)
         else:
             marks.append(0)
             
         answer12 = request.form['answer12']
         if answer12 == 'A':
-            marks.append(7.6)
+            marks.append(7.69)
         else:
             marks.append(0)
             
         answer13 = request.form['answer13']
         if answer13 == 'A':
-            marks.append(7.6)
+            marks.append(7.69)
         else:
             marks.append(0)
         
