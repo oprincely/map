283b16ed3035950a1cc12b52e5de1205f006d4f0
Oprincely <oprincely@gmail.com>
2020-09-19 08:27:00 +0100

    v9

--- app/api/num.py
+++ app/api/num.py
@@ -1,11 +1,36 @@
 from app.api import bp
 from flask import jsonify
-from app.hello import (life_number)
+from app.hello import (life_number,your_personal,hearts_d,image_num,real)
 #from app.models import User
 
-@bp.route('/api/num/<int:id>', methods=['GET'])
-def get_num_des(id):
+
+@bp.route('/api/num/<int:id>/Destiny', methods=['GET'])
+def get_destiny(id):    
+    for k, v in your_personal.items():
+        if k == id:
+            return v
+
+@bp.route('/api/num/<int:id>/Birth', methods=['GET'])
+def get_birth(id):    
     for k, v in life_number.items():
+        if k == id:
+            return v
+        
+@bp.route('/api/num/<int:id>/Heart', methods=['GET'])
+def get_heart(id):    
+    for k, v in hearts_d.items():
+        if k == id:
+            return v
+        
+@bp.route('/api/num/<int:id>/Personality', methods=['GET'])
+def get_per(id):
+    for k, v in image_num.items():
+        if k == id:
+            return v
+        
+@bp.route('/api/num/<int:id>/Reality', methods=['GET'])
+def get_real(id):    
+    for k, v in real.items():
         if k == id:
             return v
 '''
