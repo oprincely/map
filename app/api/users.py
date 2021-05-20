f245be7106f6d213e3b0bb836f9052c89ed5f116
Oprincely <oprincely@gmail.com>
2021-05-20 17:56:47 +0100

    edited users.py

--- app/api/users.py
+++ app/api/users.py
@@ -8,12 +8,12 @@ from app import db
 from app.api.errors import bad_request
 
 @bp.route('/api/users/<int:id>', methods=['GET'])
-@token_auth.login_required
+#@token_auth.login_required
 def get_user(id):
     return jsonify(User.query.get_or_404(id).to_dict())
 
 @bp.route('/api/users', methods=['GET'])
-@token_auth.login_required
+#@token_auth.login_required
 def get_users():
     page = request.args.get('page', 1, type=int)
     per_page = min(request.args.get('per_page', 10, type=int), 100)
