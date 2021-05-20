932447a046f1968f64849270bb4d624eab76bc66
Oprincely <oprincely@gmail.com>
2021-05-20 13:37:33 +0100

    edited app/api/users and app/models

--- app/api/users.py
+++ app/api/users.py
@@ -7,12 +7,12 @@ from app.api.auth import token_auth
 from app import db
 from app.api.errors import bad_request
 
-@bp.route('/users/<int:id>', methods=['GET'])
+@bp.route('/api/users/<int:id>', methods=['GET'])
 @token_auth.login_required
 def get_user(id):
     return jsonify(User.query.get_or_404(id).to_dict())
 
-@bp.route('/users', methods=['GET'])
+@bp.route('/api/users', methods=['GET'])
 @token_auth.login_required
 def get_users():
     page = request.args.get('page', 1, type=int)
@@ -20,7 +20,7 @@ def get_users():
     data = User.to_collection_dict(User.query, page, per_page, 'api.get_users')
     return jsonify(data)
 
-@bp.route('/users/<int:id>/followers', methods=['GET'])
+@bp.route('/api/users/<int:id>/followers', methods=['GET'])
 @token_auth.login_required
 def get_followers(id):
     user = User.query.get_or_404(id)
@@ -30,7 +30,7 @@ def get_followers(id):
                                    'api.get_followers', id=id)
     return jsonify(data)
 
-@bp.route('/users/<int:id>/followed', methods=['GET'])
+@bp.route('/api/users/<int:id>/followed', methods=['GET'])
 @token_auth.login_required
 def get_followed(id):
     user = User.query.get_or_404(id)
@@ -40,7 +40,7 @@ def get_followed(id):
                                    'api.get_followed', id=id)
     return jsonify(data)
 
-@bp.route('/users', methods=['POST'])
+@bp.route('/api/users', methods=['POST'])
 def create_user():
     data = request.get_json() or {}
     if 'username' not in data or 'email' not in data or 'password' not in data:
@@ -58,7 +58,7 @@ def create_user():
     response.headers['Location'] = url_for('api.get_user', id=user.id)
     return response
 
-@bp.route('/users/<int:id>', methods=['PUT'])
+@bp.route('/api/users/<int:id>', methods=['PUT'])
 @token_auth.login_required
 def update_user(id):
     user = User.query.get_or_404(id)
